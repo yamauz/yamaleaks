eb892b6e72a469355f3ea7dc81c681bd682b003c
yamau <yamauzy@gmail.com>
2019-08-12 15:59:31 +0900

    initial commit

---
+++ src/components/PageBody.js
@@ -0,0 +1,147 @@
+import React from 'react'
+import styled from 'styled-components'
+require('prismjs/themes/prism.css')
+
+const Body = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.sizes.maxWidthCentered};
+
+  h1,
+  h2,
+  h3 {
+    font-weight: 600;
+    line-height: 1.25;
+    margin: 0 0 1rem 0;
+    text-transform: capitalize;
+  }
+
+  h1 {
+    font-size: 1.5em;
+  }
+  h2 {
+    font-size: 1.25em;
+  }
+  h3 {
+    font-size: 1em;
+  }
+
+  p {
+    line-height: 1.6;
+    margin: 0 0 2em 0;
+  }
+
+  a {
+    transition: 0.2s;
+    color: ${props => props.theme.colors.base};
+    &:hover {
+      color: ${props => props.theme.colors.highlight};
+    }
+  }
+
+  del {
+    text-decoration: line-through;
+  }
+  strong {
+    font-weight: 600;
+  }
+  em {
+    font-style: italic;
+  }
+
+  ul,
+  ol {
+    margin: 0 0 2em 0;
+  }
+
+  ul {
+    li {
+      list-style: disc;
+      list-style-position: inside;
+      line-height: 1.25;
+      &:last-child {
+        margin: 0;
+      }
+    }
+  }
+
+  ol {
+    li {
+      list-style: decimal;
+      list-style-position: inside;
+      line-height: 1.25;
+      &:last-child {
+        margin: 0;
+      }
+    }
+  }
+
+  hr {
+    border-style: solid;
+    border-color: ${props => props.theme.colors.secondary};
+    margin: 0 0 2em 0;
+  }
+
+  blockquote {
+    font-style: italic;
+    border-left: 4px solid ${props => props.theme.colors.secondary};
+    padding: 0 0 0 0.5em;
+  }
+
+  pre {
+    margin: 0 0 2em 0;
+    border-radius: 2px;
+    background: ${props => props.theme.colors.secondary} !important;
+    span {
+      background: inherit !important;
+    }
+  }
+
+  table {
+    display: block;
+    overflow: auto;
+    width: 100%;
+  }
+
+  table th {
+    font-weight: 600;
+  }
+
+  table td,
+  table th {
+    border: 1px solid #dfe2e5;
+    padding: 6px 13px;
+  }
+
+  table tr {
+    background-color: #fff;
+    border-top: 1px solid #c6cbd1;
+  }
+
+  table tr:nth-child(2n) {
+    background-color: #f6f8fa;
+  }
+
+  kbd {
+    background-color: #fafbfc;
+    border: 1px solid #c6cbd1;
+    border-bottom-color: #959da5;
+    border-radius: 3px;
+    box-shadow: inset 0 -1px 0 #959da5;
+    color: #444d56;
+    display: inline-block;
+    font-size: 11px;
+    line-height: 10px;
+    padding: 3px 5px;
+    vertical-align: middle;
+  }
+`
+
+const PageBody = props => {
+  return (
+    <Body
+      dangerouslySetInnerHTML={{ __html: props.body.childMarkdownRemark.html }}
+    />
+  )
+}
+
+export default PageBody
